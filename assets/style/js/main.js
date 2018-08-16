c6d775c8d98414dce6db37fbb2105b92f6e56b50
duy pham <duy@passcareer.io>
2018-08-16 16:21:04 +0700

    Implement Navbar and Header UI

--- assets/style/js/main.js
+++ assets/style/js/main.js
@@ -1,1 +1,26 @@
-ECHO is on.
+$(document).ready(() => {
+
+  // ************************ NAVIGATION *****************************
+
+  // Close Responsive menu when a link is clicked
+  $('.nav-link').click(function () {
+    $('.navbar-collapse').collapse('hide');
+  })
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#navbar',
+    offset: 75
+  })
+
+  // ON Scroll Down if the offset is higher than 50px add navbar-shrink class
+  // Else remove that class
+  $(window).scroll(function () {
+    if ($('#navbar').offset().top > 50) {
+      $('#navbar').addClass('navbar-shrink');
+    } else {
+      $('#navbar').removeClass('navbar-shrink')
+    }
+  })
+
+})
