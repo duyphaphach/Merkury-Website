996a6d5e5689c8e35d87693a0a1fc5db5e2a17db
duy pham <duy@passcareer.io>
2018-08-18 01:47:24 +0700

    Code Optimization, add animation, fix UI bug

--- assets/style/js/main.js
+++ assets/style/js/main.js
@@ -37,11 +37,54 @@ $(document).ready(() => {
       0: {
         items: 1
       },
+
       600: {
         items: 3
       },
       1000: {
         items: 5
+      }
+    }
+  })
+
+  // ANIMATION CONTROL
+
+  // Flash button
+  $('#flashbtn').hover(function () {
+    $('#flashbtn').removeClass('infinite');
+  }, function () {
+    $('#flashbtn').addClass('infinite');
+  })
+
+  // Show mission section
+  $('#mission').mouseenter(function () {
+    $('#mission').css('opacity', '1');
+  })
+
+  // BLOG SLIDER JavaScript
+  $('#blog-slider').owlCarousel({
+    margin: 15,
+    smartSpeed: 500,
+    dots: false,
+    dotsEach: false,
+    nav: true,
+    navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+    loop: true,
+    autoplay: true,
+    mouseDrag: true,
+    touchDrag: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      500: {
+        items: 2
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 4
       }
     }
   })
