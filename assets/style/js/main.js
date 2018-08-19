b6d54360aa5e2faadba4bb2f3e11dd4c01988a5d
duy pham <duy@passcareer.io>
2018-08-19 12:23:01 +0700

    Add some effect to the navbar to make it more interactive

--- assets/style/js/main.js
+++ assets/style/js/main.js
@@ -1,128 +1,27 @@
 $(document).ready(() => {
 
-  // ************************ NAVIGATION *****************************
+  // ***************************** NAVIGATION ***********************************
 
-  // Close Responsive menu when a link is clicked
+  // Close navbar after an item is clicked - The collapse() is from bootstrap.min.js
   $('.nav-link').click(function () {
     $('.navbar-collapse').collapse('hide');
   })
 
-  // Activate scrollspy to add active class to navbar items on scroll
+  // Active Scrollspy
   $('body').scrollspy({
-    target: '#navbar',
-    offset: 75
+    target: '#main-nav',
+    offset: 75,
   })
 
-  // ON Scroll Down if the offset is higher than 50px add navbar-shrink class
-  // Else remove that class
+  // Shrink navbar if the offset is higher than 50 px
   $(window).scroll(function () {
-    if ($('#navbar').offset().top > 50) {
-      $('#navbar').addClass('navbar-shrink');
+    console.log("1");
+    if ($('#main-nav').offset().top > 50) {
+      $('#main-nav').addClass('navbar-shrink');
     } else {
-      $('#navbar').removeClass('navbar-shrink');
-    }
-  })
-  // MEMBERS SLIDER JavaScript
-  $('#members-slider').owlCarousel({
-    margin: 25,
-    smartSpeed: 500,
-    nav: false,
-    dots: true,
-    dotsEach: true,
-    loop: true,
-    autoplay: true,
-    mouseDrag: true,
-    touchDrag: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-
-      600: {
-        items: 3
-      },
-      1000: {
-        items: 5
-      }
+      $('#main-nav').removeClass('navbar-shrink')
     }
   })
 
-  // ANIMATION CONTROL
 
-  // Flash button
-  $('#flashbtn').hover(function () {
-    $('#flashbtn').removeClass('infinite');
-  }, function () {
-    $('#flashbtn').addClass('infinite');
-  })
-
-  // Show mission section
-  $('#mission').mouseenter(function () {
-    $('#mission').css('opacity', '1');
-  })
-
-  // SMOOTH SCROLL
-  // Select all links with hashes
-  $('a[href*="#"]')
-    // Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function (event) {
-      // On-page links
-      if (
-        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-        location.hostname == this.hostname
-      ) {
-        // Figure out element to scroll to
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        // Does a scroll target exist?
-        if (target.length) {
-          // Only prevent default if animation is actually gonna happen
-          event.preventDefault();
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 1000, function () {
-            // Callback after animation
-            // Must change focus!
-            var $target = $(target);
-            $target.focus();
-            if ($target.is(":focus")) { // Checking if the target was focused
-              return false;
-            } else {
-              $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-              $target.focus(); // Set focus again
-            };
-          });
-        }
-      }
-    });
-
-  // BLOG SLIDER JavaScript
-  $('#blog-slider').owlCarousel({
-    margin: 15,
-    smartSpeed: 500,
-    dots: false,
-    dotsEach: false,
-    nav: true,
-    navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-    loop: true,
-    autoplay: true,
-    mouseDrag: true,
-    touchDrag: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      500: {
-        items: 2
-      },
-      600: {
-        items: 3
-      },
-      1000: {
-        items: 4
-      }
-    }
-  })
 })
